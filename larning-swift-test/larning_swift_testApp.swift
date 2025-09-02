b20b4620679fc1a288d2e6dd57f2bf310628fa8b
Y.Tsubasa <125607203+ytsubasa@users.noreply.github.com>
2025-09-02 13:58:27 +0900

    初期コミット

---
+++ larning-swift-test/larning_swift_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  larning_swift_testApp.swift
+//  larning-swift-test
+//
+//  Created by 吉田翔 on 2025/09/02.
+//
+
+import SwiftUI
+
+@main
+struct larning_swift_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
